4d560d9981a2c13bf11310bbfeadee649318de9c
grshx <shinde.girish.r@gmail.com>
2025-01-24 11:44:54 +0100

    check if string is palindrome

---
+++ palindrome/go.mod
@@ -0,0 +1,3 @@
+module github.com/grshx/go-hands-on/palindrome
+
+go 1.23.5
---
+++ palindrome/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func isPalindrome(s string) bool {
+	strLenth := len(s)
+	for i := 0; i < strLenth/2; i++ {
+		if s[i] != s[strLenth-1-i] {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+
+	var input string
+
+	fmt.Println("Enter a string:")
+	fmt.Scanln(&input)
+
+	if isPalindrome(input) {
+		fmt.Println("The string is a palindrome")
+	} else {
+		fmt.Println("The string is not a palindrome")
+	}
+
+}
