50fb311b1a73efa34eb7e146dd7945c606b0fd45
grshx <shinde.girish.r@gmail.com>
2025-01-24 01:07:46 +0100

    cli word counter

---
+++ cli-word-counter/go.mod
@@ -0,0 +1,3 @@
+module github.com/grshx/go-hands-on/cli-word-counter
+
+go 1.23.5
---
+++ cli-word-counter/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	fileFlag := flag.String("file", "", "path of the file to count words in")
+	flag.Parse()
+
+	var text string
+	var scanner *bufio.Scanner
+
+	if *fileFlag == "" {
+		fmt.Println("Enter text to count words (press Ctrl+D to finish):")
+		scanner = bufio.NewScanner(os.Stdin)
+	} else {
+		file, err := os.Open(*fileFlag)
+		if err != nil {
+			fmt.Printf("something went wrong while opening the file \n%v", err.Error())
+		}
+		defer file.Close()
+		scanner = bufio.NewScanner(file)
+	}
+
+	for scanner.Scan() {
+		text += strings.TrimSpace(scanner.Text()) + " "
+	}
+
+	err := scanner.Err()
+
+	if err != nil {
+		fmt.Printf("something went wrong while scanning text \n %v", err.Error())
+		return
+	}
+
+	wordCount := 0
+	for _, word := range strings.Split(text, " ") {
+		if strings.TrimSpace(word) == "" {
+			continue
+		}
+		wordCount++
+	}
+
+	fmt.Println("total word count : ", wordCount)
+}
